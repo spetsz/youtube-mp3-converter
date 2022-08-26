26e3cae2ae4dd1808dae0c3dd50c7e33a16e7433
Spetsz_ <anewadult@gmail.com>
2022-08-26 14:04:01 +0200

    Nav component finilized

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {BsYoutube} from 'react-icons/bs'
 import styles from '../styles/nav.module.css'
 
-const {nav, container, logo, nav_menu, nav_item, nav_link} = styles
+
+const {nav, container, nav_brand, nav_logo, nav_menu, nav_item, nav_link} = styles
 
 const Nav = () => {
   return (
     <nav className={nav}>
         <div className={container}>
-            <div className={logo}>
-                
+            
+            <div className={nav_brand}>
+                <BsYoutube className={nav_logo}/>
+                <Link to="/" className={nav_link}>Youtube mp3</Link>
             </div>
+          
             <ul className={nav_menu}>
                 <li className={nav_item}>
-                    <a href="/" className={nav_link}>Home</a>
+                    <Link to="/" className={nav_link}>Home</Link>
                 </li>
                 <li className={nav_item}>
-                    <a href="/about" className={nav_link}>About</a>
+                    <Link to="/about" className={nav_link}>About</Link>
                 </li>
             </ul>
         </div>
